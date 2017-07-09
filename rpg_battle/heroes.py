c2a8519734a0d2e76755edf3293de10b63edf6b9
Jason Symons <jason.symons@gmail.com>
2017-07-09 10:54:15 -0400

    tabs to spaces

--- rpg_battle/heroes.py
+++ rpg_battle/heroes.py
@@ -1,140 +1,140 @@
 from .exceptions import *
 
 class Hero(object):
-	
-	def __init__(self, level=1):
-		"""
-		Sets stats up and levels up hero if necessary.
-		"""
-		pass
+    
+    def __init__(self, level=1):
+        """
+        Sets stats up and levels up hero if necessary.
+        """
+        pass
 
-	def xp_for_next_level(self):
-		"""
-		Returns the number of xp at which the next level is gained.
-		By default this should be 10 times current level, so 10 for
-		level 1, 20 for level 2, etc.
-		"""
-		pass
+    def xp_for_next_level(self):
+        """
+        Returns the number of xp at which the next level is gained.
+        By default this should be 10 times current level, so 10 for
+        level 1, 20 for level 2, etc.
+        """
+        pass
 
-	def fight(self, target):
-		"""
-		Attacks target, dealing damage equal to the user's strength.
-		"""
-		pass
+    def fight(self, target):
+        """
+        Attacks target, dealing damage equal to the user's strength.
+        """
+        pass
 
-	def gain_xp(self, xp):
-		"""
-		Increases current xp total, triggers level up if necessary,
-		rolling over any excess xp. Example: If a level 1 hero received
-		enough xp to increase its total to 12 xp it would level up and
-		then have an xp total of 2.
-		"""
-		pass
-	def take_damage(self, damage):
-		"""
-		Reduce hp by damage taken.
-		"""
-		pass
+    def gain_xp(self, xp):
+        """
+        Increases current xp total, triggers level up if necessary,
+        rolling over any excess xp. Example: If a level 1 hero received
+        enough xp to increase its total to 12 xp it would level up and
+        then have an xp total of 2.
+        """
+        pass
+    def take_damage(self, damage):
+        """
+        Reduce hp by damage taken.
+        """
+        pass
 
-	def heal_damage(self, healing):
-		"""
-		Increase hp by healing but not exceeding maxhp
-		"""
-		pass
+    def heal_damage(self, healing):
+        """
+        Increase hp by healing but not exceeding maxhp
+        """
+        pass
 
-	def is_dead(self):
-		"""
-		Returns True if out of hp
-		"""
-		pass
+    def is_dead(self):
+        """
+        Returns True if out of hp
+        """
+        pass
 
 
 class Warrior(Hero):
-	"""
-	Stat modifiers:
-	strength +1
-	intelligence -2
-	constitution +2
-	speed -1
-	"""
-	
-	def shield_slam(self, target):
-		"""
-		cost: 5 mp
-		damage: 1.5 * strength
-		"""
-		pass
+    """
+    Stat modifiers:
+    strength +1
+    intelligence -2
+    constitution +2
+    speed -1
+    """
+    
+    def shield_slam(self, target):
+        """
+        cost: 5 mp
+        damage: 1.5 * strength
+        """
+        pass
 
-	def reckless_charge(self, target):
-		"""
-		cost: 4 hp
-		damage: 1.5 * strength
-		"""
-		pass
+    def reckless_charge(self, target):
+        """
+        cost: 4 hp
+        damage: 1.5 * strength
+        """
+        pass
 
 class Mage(Hero):
-	"""
-	Stat modifiers
-	strength -2
-	inteligence +3
-	constitution -2
-	"""
+    """
+    Stat modifiers
+    strength -2
+    inteligence +3
+    constitution -2
+    """
 
-	def fireball(self, target):
-		"""
-		cost: 8 mp
-		damage: 6 + (0.5 * intelligence)
-		"""
-		pass
+    def fireball(self, target):
+        """
+        cost: 8 mp
+        damage: 6 + (0.5 * intelligence)
+        """
+        pass
 
-	def frostbolt(self, target):
-		"""
-		cost: 3 mp
-		damage: 3 + level
-		"""
-		pass
+    def frostbolt(self, target):
+        """
+        cost: 3 mp
+        damage: 3 + level
+        """
+        pass
 
 class Cleric(Hero):
-	"""
-	Stat modifiers:
-	speed -1
-	constitution +1
-	"""
+    """
+    Stat modifiers:
+    speed -1
+    constitution +1
+    """
 
-	def heal(self, target):
-		"""
-		cost: 4 mp
-		healing: constitution
-		"""
-		pass
+    def heal(self, target):
+        """
+        cost: 4 mp
+        healing: constitution
+        """
+        pass
 
-	def smite(self, target):
-		"""
-		cost: 7 mp
-		damage: 4 + (0.5 * (intelligence + constitution))
-		"""
-		pass
+    def smite(self, target):
+        """
+        cost: 7 mp
+        damage: 4 + (0.5 * (intelligence + constitution))
+        """
+        pass
 
 class Rogue(Hero):
-	"""
-	Stat modifiers:
-	speed +2
-	strength +1
-	intelligence -1
-	constitution -2
-	"""
+    """
+    Stat modifiers:
+    speed +2
+    strength +1
+    intelligence -1
+    constitution -2
+    """
 
-	def backstab(self, target):
-		"""
-		cost: None
-		restriction: target must be undamaged, else raise InvalidTarget
-		damage: 2 * strength
-		"""
-		pass
+    def backstab(self, target):
+        """
+        cost: None
+        restriction: target must be undamaged, else raise InvalidTarget
+        damage: 2 * strength
+        """
+        pass
 
-	def rapid_strike(self, target):
-		"""
-		cost: 5 mp
-		damage: 4 + speed
-		"""
-		pass
+    def rapid_strike(self, target):
+        """
+        cost: 5 mp
+        damage: 4 + speed
+        """
+        pass
--- rpg_battle/monsters.py
+++ rpg_battle/monsters.py
@@ -1,172 +1,172 @@
 from .exceptions import *
 
 class Monster(object):
-	
-	def __init__(self, level=1):
-		"""
-		Sets up stats and levels up the monster if necessary
-		"""
-		pass
+    
+    def __init__(self, level=1):
+        """
+        Sets up stats and levels up the monster if necessary
+        """
+        pass
 
-	def xp(self):
-		"""
-		Returns the xp value of monster if defeated.
-		XP value formula: (average of stats) + (maxhp % 10)
-		"""
-		pass
+    def xp(self):
+        """
+        Returns the xp value of monster if defeated.
+        XP value formula: (average of stats) + (maxhp % 10)
+        """
+        pass
 
-	def fight(self, target):
-		"""
-		Attacks target dealing damage equal to strength
-		"""
-		pass
+    def fight(self, target):
+        """
+        Attacks target dealing damage equal to strength
+        """
+        pass
 
 
-	def take_damage(self, damage):
-		"""
-		Reduce hp by damage taken.
-		"""
-		pass
+    def take_damage(self, damage):
+        """
+        Reduce hp by damage taken.
+        """
+        pass
 
-	def heal_damage(self, healing):
-		"""
-		Increase hp by healing but not exceeding maxhp
-		"""
-		pass
+    def heal_damage(self, healing):
+        """
+        Increase hp by healing but not exceeding maxhp
+        """
+        pass
 
-	def is_dead(self):
-		"""
-		Returns True if out of hp
-		"""
-		pass
+    def is_dead(self):
+        """
+        Returns True if out of hp
+        """
+        pass
 
-	def attack(self, target):
-		"""
-		Attacks target using next ability in command queue
-		"""
-		pass
+    def attack(self, target):
+        """
+        Attacks target using next ability in command queue
+        """
+        pass
 
 
 class Dragon(Monster):
-	"""
-	base hp: 100
-	constitution multiplier: 2
-	special feature: Reduce all damage taken by 5
-	"""
+    """
+    base hp: 100
+    constitution multiplier: 2
+    special feature: Reduce all damage taken by 5
+    """
 
-	def tail_swipe(self, target):
-		"""
-		damage: strength + speed
-		"""
-		pass
+    def tail_swipe(self, target):
+        """
+        damage: strength + speed
+        """
+        pass
 
 
 class RedDragon(Dragon):
-	"""
-	strength multiplier: 2
-	intelligence multiplier: 1.5
-	command queue: fire_breath, tail_swipe, fight
-	"""
+    """
+    strength multiplier: 2
+    intelligence multiplier: 1.5
+    command queue: fire_breath, tail_swipe, fight
+    """
 
-	def fire_breath(self, target):
-		"""
-		damage: intelligence * 2.5
-		"""
-		pass
+    def fire_breath(self, target):
+        """
+        damage: intelligence * 2.5
+        """
+        pass
 
 
 class GreenDragon(Dragon):
-	"""
-	strength multiplier: 1.5
-	speed multiplier: 1.5
-	command queue: poison_breath, tail_swipe, fight
-	"""
-	
-	def poison_breath(self, target):
-		"""
-		damage: (intelligence + constitution) * 1.5
-		"""
-		pass
+    """
+    strength multiplier: 1.5
+    speed multiplier: 1.5
+    command queue: poison_breath, tail_swipe, fight
+    """
+    
+    def poison_breath(self, target):
+        """
+        damage: (intelligence + constitution) * 1.5
+        """
+        pass
 
 
 class Undead(Monster):
-	"""
-	constitution multiplier: 0.25
-	special feature: undead take damage from healing except their own healing abilities
-	"""
+    """
+    constitution multiplier: 0.25
+    special feature: undead take damage from healing except their own healing abilities
+    """
 
-	def life_drain(self, target):
-		"""
-		damage: intelligence * 1.5
-		heals unit for damage done
-		"""
-		pass
+    def life_drain(self, target):
+        """
+        damage: intelligence * 1.5
+        heals unit for damage done
+        """
+        pass
 
 
 class Vampire(Undead):
-	"""
-	base hp: 30
-	intelligence multiplier: 2
-	command queue: fight, bite, life_drain
-	"""
-	
-	def bite(self, target):
-		"""
-		damage: speed * 0.5
-		also reduces target's maxhp by amount equal to damage done
-		heals unit for damage done
-		"""
-		pass
+    """
+    base hp: 30
+    intelligence multiplier: 2
+    command queue: fight, bite, life_drain
+    """
+    
+    def bite(self, target):
+        """
+        damage: speed * 0.5
+        also reduces target's maxhp by amount equal to damage done
+        heals unit for damage done
+        """
+        pass
 
 
 class Skeleton(Undead):
-	"""
-	strength multiplier: 1.25
-	speed multiplier: 0.5
-	intelligence multiplier: 0.25
-	command queue: bash, fight, life_drain
-	"""
-	
-	def bash(self, target):
-		"""
-		damage: strength * 2
-		"""
-		pass
+    """
+    strength multiplier: 1.25
+    speed multiplier: 0.5
+    intelligence multiplier: 0.25
+    command queue: bash, fight, life_drain
+    """
+    
+    def bash(self, target):
+        """
+        damage: strength * 2
+        """
+        pass
 
 
 class Humanoid(Monster):
-	def slash(self, target):
-		"""
-		damage: strength + speed
-		"""
-		pass
+    def slash(self, target):
+        """
+        damage: strength + speed
+        """
+        pass
 
 
 class Troll(Humanoid):
-	"""
-	strength multiplier: 1.75
-	constitution multiplier: 1.5
-	base hp: 20
-	"""
-	
-	def regenerate(self, *args):
-		"""
-		heals self for constitution
-		"""
-		pass
+    """
+    strength multiplier: 1.75
+    constitution multiplier: 1.5
+    base hp: 20
+    """
+    
+    def regenerate(self, *args):
+        """
+        heals self for constitution
+        """
+        pass
 
 
 class Orc(Humanoid):
-	"""
-	strength multiplier: 1.75
-	base hp: 16
-	"""
-	
-	def blood_rage(self, target):
-		"""
-		cost: constitution * 0.5 hp
-		damage: strength * 2
-		"""
-		pass
+    """
+    strength multiplier: 1.75
+    base hp: 16
+    """
+    
+    def blood_rage(self, target):
+        """
+        cost: constitution * 0.5 hp
+        damage: strength * 2
+        """
+        pass
 
-		
+        
