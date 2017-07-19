0c0c9b2a435f81fb08d129df9cf03fd252c10793
ry-werth <ryan@draftboard.com>
2017-07-19 12:39:12 -0700

    Update monsters.py

--- rpg_battle/monsters.py
+++ rpg_battle/monsters.py
@@ -2,43 +2,60 @@ from .exceptions import *
 
 class Monster(object):
     
+    strMult = 1
+    constMult = 1
+    intMult = 1
+    spdMult = 1
+    init_hp = 10
+    
     def __init__(self, level=1):
         """
         Sets up stats and levels up the monster if necessary
         """
-        pass
+        self.level = level
+        self.strength = (7 + self.level) * self.strMult
+        self.constitution = (7 + self.level) * self.constMult
+        self.intelligence = (7 + self.level) * self.intMult
+        self.speed = (7 + self.level) * self.spdMult
+        self.maxhp = int(self.init_hp + (self.level - 1) * (0.5 * self.constitution))
+        self.hp = self.maxhp
+        
 
     def xp(self):
         """
         Returns the xp value of monster if defeated.
         XP value formula: (average of stats) + (maxhp % 10)
         """
-        pass
+        xp = (self.strength + self.constitution + self.intelligence + self.speed)/4 + self.maxhp%10
+        return xp
+        
 
     def fight(self, target):
         """
         Attacks target dealing damage equal to strength
         """
-        pass
+        target.take_damage(self.strength)
 
 
     def take_damage(self, damage):
         """
         Reduce hp by damage taken.
         """
-        pass
+        self.hp -= damage
 
     def heal_damage(self, healing):
         """
         Increase hp by healing but not exceeding maxhp
         """
-        pass
+        self.hp += healing
+        if self.hp > self.maxhp:
+            self.hp = self.maxhp
 
     def is_dead(self):
         """
         Returns True if out of hp
         """
-        pass
+        return self.hp <= 0
 
     def attack(self, target):
         """
@@ -53,12 +70,23 @@ class Dragon(Monster):
     constitution multiplier: 2
     special feature: Reduce all damage taken by 5
     """
+    constMult = 2
+    init_hp = 100
+    
 
     def tail_swipe(self, target):
         """
         damage: strength + speed
         """
-        pass
+        target.take_damage(self.strength + self.speed)
+    
+    def take_damage(self, damage):
+        """
+        Reduce hp by damage taken.
+        """
+        damage -= 5
+        if (damage > 0): 
+            self.hp -= damage
 
 
 class RedDragon(Dragon):
@@ -67,12 +95,15 @@ class RedDragon(Dragon):
     intelligence multiplier: 1.5
     command queue: fire_breath, tail_swipe, fight
     """
+    strMult = 2
+    intMult = 1.5
+    command_q = ['fire_breath', 'tail_swipe', 'fight']
 
     def fire_breath(self, target):
         """
         damage: intelligence * 2.5
         """
-        pass
+        target.take_damage(self.intelligence * 2.5)
 
 
 class GreenDragon(Dragon):
@@ -81,12 +112,15 @@ class GreenDragon(Dragon):
     speed multiplier: 1.5
     command queue: poison_breath, tail_swipe, fight
     """
+    strMult = 1.5
+    spdMult = 1.5
+    command_q = ['poison_breath', 'tail_swipe', 'fight']
     
     def poison_breath(self, target):
         """
         damage: (intelligence + constitution) * 1.5
         """
-        pass
+        target.take_damage((self.intelligence + self.constitution) * 1.5)
 
 
 class Undead(Monster):
@@ -94,13 +128,22 @@ class Undead(Monster):
     constitution multiplier: 0.25
     special feature: undead take damage from healing except their own healing abilities
     """
+    constMult = 0.25
+        
 
     def life_drain(self, target):
         """
         damage: intelligence * 1.5
         heals unit for damage done
         """
-        pass
+        target.take_damage(self.intelligence * 1.5)
+        Monster.heal_damage(self, self.intelligence * 1.5)
+        
+    def heal_damage(self, healing):
+        """
+        decreases hp by healing 
+        """
+        self.hp -= healing
 
 
 class Vampire(Undead):
@@ -110,13 +153,21 @@ class Vampire(Undead):
     command queue: fight, bite, life_drain
     """
     
+    init_hp = 30
+    intMult = 2
+    command_q = ['fight', 'bite', 'life_drain']
+    
     def bite(self, target):
         """
         damage: speed * 0.5
         also reduces target's maxhp by amount equal to damage done
         heals unit for damage done
         """
-        pass
+        bite_damage = self.speed * 0.5
+        target.take_damage(bite_damage)
+        target.maxhp -= bite_damage
+        Monster.heal_damage(self, bite_damage)
+        
 
 
 class Skeleton(Undead):
@@ -126,20 +177,25 @@ class Skeleton(Undead):
     intelligence multiplier: 0.25
     command queue: bash, fight, life_drain
     """
+    strMult = 1.25
+    spdMult = 0.5
+    intMult = 0.25
+    command_q = ['bash', 'fight', 'life_drain']
     
     def bash(self, target):
         """
         damage: strength * 2
         """
-        pass
+        target.take_damage(self.strength * 2)
 
 
 class Humanoid(Monster):
+    
     def slash(self, target):
         """
         damage: strength + speed
         """
-        pass
+        target.take_damage(self.strength + self.speed)
 
 
 class Troll(Humanoid):
@@ -148,12 +204,17 @@ class Troll(Humanoid):
     constitution multiplier: 1.5
     base hp: 20
     """
+    strMult = 1.75
+    constMult = 1.5
+    init_hp = 20
+    command_q =  ['slash', 'fight', 'regenerate']
+    
     
     def regenerate(self, *args):
         """
         heals self for constitution
         """
-        pass
+        self.hp += self.constitution
 
 
 class Orc(Humanoid):
@@ -161,12 +222,14 @@ class Orc(Humanoid):
     strength multiplier: 1.75
     base hp: 16
     """
+    strMult = 1.75
+    init_hp = 16
+    command_q =  ['blood_rage', 'slash', 'fight']
     
     def blood_rage(self, target):
         """
         cost: constitution * 0.5 hp
         damage: strength * 2
         """
-        pass
-
-        
+        target.take_damage(self.strength * 2)
+        self.hp -= (self.constitution * 0.5)
